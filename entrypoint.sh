1af22ec50a06bb4d112d6f54e22bbe80a62b0c04
Sandro Giacomozzi <sandro.giacomozzi@grupoboticario.com.br>
2024-03-07 14:22:07 -0300

    update project

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,17 @@ echo "$(date +'%H:%M:%S.%3N') Call jar file"
 cd /home/ubuntu
 
 # Check whether crac-files folder is present not empty and if so, restore from checkpoint, otherwise start from jar
-if [ -d "crac-files" ]; then
-  if [ "$(ls -A crac-files)" ]; then
+if [ -d "crac-files" -a "$(ls -A crac-files)" ]; then
+  #if [ "$(ls -A crac-files)" ]; then
     echo "Restore from checkpoint"
     java -XX:CRaCRestoreFrom=crac-files
-  else
-    echo "Standard start from jar file"
-    java -XX:CRaCCheckpointTo=crac-files -jar demo.jar
-  fi
+  #else
+  #  echo "Standard start from jar file"
+  #  echo 199 > /proc/sys/kernel/ns_last_pid; java -XX:CRaCCheckpointTo=crac-files -jar demo.jar
+  #fi
 else
-  echo "No crac-files folder found"
-  mkdir crac-files
+  #echo "No crac-files folder found"
+  #mkdir crac-files
   echo "Standard start from jar file"
-  java -XX:CRaCCheckpointTo=crac-files -jar demo.jar
+  echo 199 > /proc/sys/kernel/ns_last_pid; java -XX:CRaCCheckpointTo=crac-files -jar demo.jar
 fi
